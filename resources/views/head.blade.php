1ef65747d4dd1e98560d09d375f709220c4b21ea
jingyang <linshuboy@qq.com>
2020-03-23 16:53:40 +0800

    support xiumi

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,4 +1,6 @@
 <script src="{!!asset('/laravel-u-editor/ueditor.config.js')!!}"></script>
 <script src="{!!asset('/laravel-u-editor/ueditor.all.min.js')!!}"></script>
+<script src="{!!asset('/laravel-u-editor/xiumi-ue-dialog-v5.js')!!}"></script>
+<link rel="stylesheet" href="{!!asset('/laravel-u-editor/xiumi-ue-v5.css')!!}">
 {{-- 载入语言文件,根据laravel的语言设置自动载入 --}}
 <script src="{!!asset($UeditorLangFile)!!}"></script>

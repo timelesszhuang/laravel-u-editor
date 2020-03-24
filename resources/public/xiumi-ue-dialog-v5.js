256bdd6bb67859abee0b84467ca286c9d33a0dcf
jingyang <linshuboy@qq.com>
2020-03-24 11:42:09 +0800

    解决秀米报错

--- resources/public/xiumi-ue-dialog-v5.js
+++ resources/public/xiumi-ue-dialog-v5.js
@@ -7,7 +7,7 @@ UE.registerUI('dialog', function (editor, uiName) {
         title  : '秀米',
         onclick: function () {
             var dialog = new UE.ui.Dialog({
-                iframeUrl: 'xiumi-ue-dialog-v5.html',
+                iframeUrl: '/laravel-u-editor/xiumi-ue-dialog-v5.html',
                 editor   : editor,
                 name     : 'xiumi-connect',
                 title    : "秀米图文消息助手",

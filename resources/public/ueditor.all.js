e68ca25c0f6210d16b206f1db64ec5750d166b41
jingyang <linshuboy@qq.com>
2020-04-01 09:51:24 +0800

    解决u-editor在bootstrap弹出model全屏不显示问题,解决u-editor在bootstrap弹出model弹出组件框不显示问题

--- resources/public/ueditor.all.js
+++ resources/public/ueditor.all.js
@@ -29063,7 +29063,13 @@ UE.ui = baidu.editor.ui = {};
                     while (container.tagName != "BODY") {
                         var position = baidu.editor.dom.domUtils.getComputedStyle(container, "position");
                         nodeStack.push(position);
-                        container.style.position = "static";
+                        var isModal = false;
+                        //判断该dom是否为modal
+                        var classes = $(container).attr("class");
+                        if (classes !== undefined) {classes =classes.split(" ");
+                            //将class类名按空格分成数组，便于找到modal
+                            for (var i = 0;i < classes.length; i++) {if(classes[i] == "modal") {isModal= true;}}}//如果是modal,则不设置position为static
+                        if (!isModal) {container.style.position = "static";}
                         container = container.parentNode;
                     }
                     this._bakHtmlOverflow = document.documentElement.style.overflow;
